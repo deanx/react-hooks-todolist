e15b95ffd23a28dfaf1c392418163e998adda2be
Alex Costa <alex@deanx.com.br>
2018-12-15 18:57:19 +0100

    Todo form with hooks

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import Typography from '@material-ui/core/Typography';
 import './styles.css';
 
+import TodoForm from './TodoForm';
+
 const App = () => {
   return (
     <div className="App">
       <Typography component="h1" variant="h2">
         Todos
       </Typography>
+      <TodoForm saveTodo={console.warn}/>
     </div>
   );
 };
